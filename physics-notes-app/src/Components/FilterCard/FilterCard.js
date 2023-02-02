732e8fc4b24526c5d206ed24e37c7acc9a97d469
Shaowy05 <jamesshao05@hotmail.com>
2023-02-02 18:26:39 +0000

    Commit 18: Added margin below filter card

--- physics-notes-app/src/Components/FilterCard/FilterCard.js
+++ physics-notes-app/src/Components/FilterCard/FilterCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './FilterCard.css';
 
 import Card from 'react-bootstrap/Card';
 import Collapse from 'react-bootstrap/Collapse';
@@ -25,25 +26,27 @@ export default class FilterCard extends React.Component {
         const { open } = this.state;
 
         return(
-            <Card>
-                <Card.Header 
-                    onClick={() => this.setState({ open: !open })}
-                    aria-controls="filter-settings"
-                    aria-expanded={open}
-                    style={{
-                        cursor: 'pointer',
-                        display: 'flex',
-                        justifyContent: 'center'
-                    }}
-                >
-                    <p id="filter-card-header-text">Filters</p>
-                    </Card.Header>
-                <Collapse in={open}>
-                    <Card.Text>
-                        Filter text
-                    </Card.Text>
-                </Collapse>
-            </Card>
+            <div>
+                <Card>
+                    <Card.Header 
+                        onClick={() => this.setState({ open: !open })}
+                        aria-controls="filter-settings"
+                        aria-expanded={open}
+                        style={{
+                            cursor: 'pointer',
+                            display: 'flex',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <p id="filter-card-header-text">Filters</p>
+                        </Card.Header>
+                    <Collapse in={open}>
+                        <Card.Text>
+                            Filter text
+                        </Card.Text>
+                    </Collapse>
+                </Card>
+            </div>
         );
 
     }
