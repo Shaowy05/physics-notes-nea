3204bd5b9f57a50896f528f81e2b4bd4e2cdf456
Shaowy05 <jamesshao05@hotmail.com>
2023-03-14 21:30:22 +0000

    Annotation

--- physics-notes-app/src/Components/AddNotesCard/AddNotesCard.js
+++ physics-notes-app/src/Components/AddNotesCard/AddNotesCard.js
@@ -10,7 +10,8 @@ export default class AddNotesCard extends React.Component {
     constructor(props) {
         super(props);
 
-        // In the state we have fields for the ti
+        // In the state we have fields for the title, the JPG file, URL for the notes and a state which
+        // can be used to show an error if an error occurs.
         this.state = {
 
             notesTitle: '',
@@ -26,18 +27,38 @@ export default class AddNotesCard extends React.Component {
 
     }
 
+    // Setting the title state upon the user entering input.
     handleTitleInput = event => this.setState({ notesTitle: event.target.value });
 
+    // Here we set the state to the file stored in the HTML tag using the .files property.
     handleAddNotes = event => {
         this.setState({ notes: event.target.files[0] });
     }
 
+    // This is the method that is executed upon the user pressing the add notes button, handling the
+    // transfer of information to the backend.
     handleNotesUpload = event => {
+        // First we create a new instance of FileReader.
         const reader = new FileReader()
-        reader.readAsDataURL(this.state.notes);
+        // Then we use this instance, plus the readAsDataUrl method to convert the JPG stored in the
+        // state to turn the file into a base 64 result. We wrap this in a try catch in the instance
+        // that the user enters a non-readable format.
+        try {
+            reader.readAsDataURL(this.state.notes);
+        }
+        catch(err) {
+            this.setState({ failedToUpload: true });
+            return null;
+        }
 
+        // Now we create an asynchronous function so that we can upload the file to the backend, without
+        // having any blocking code on the main website.
         reader.onload = async () => {
+            // Getting the URL representation from the result of the reader.
             const base64Notes = reader.result;
+            // Once this has done, we send a POST request to the notes endpoint of the API, passing
+            // in the title, the URL of the notes, the ID of the current user and finally the ID of
+            // folder on top of the Folder Path Stack.
             fetch('http://localhost:3000/notes', {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
@@ -48,35 +69,46 @@ export default class AddNotesCard extends React.Component {
                     folderId: this.props.currentFolder.id
                 }) 
             })
+            // Convert the response to a Javascript object.
             .then(response => response.json())
+            // When the API sends a response...
             .then(message => {
-                console.log(message)
+                // If the API responds with a success status of true, then we can run the updateFolderHasNotes
+                // method to make sure that all the parent folders of the new set of notes have a hasNotes
+                // property of true, passing in the ID of the current folder.
                 if (message.success) {
                     this.updateFolderHasNotes(this.props.currentFolder.id);
+                    // Once this has been done successfully, we can reroute the user to the index page.
                     this.props.changeRoute('index');
                 }
+                // Otherwise, we can utilise the failedToUpload state to be true and provide some visual
+                // feedback to the user.
                 else {
                     this.setState({ failedToUpload: true });
                 }
             })
+            // If there was an error at any point then we set the failedToUpload state to be true as
+            // well.
             .catch(err => {
-                console.log(err);
                 this.setState({ failedToUpload: true });
             });
         }
-
     }
 
-    // This function will recursively update the current folder, and all of its
-    // parent folders' has_notes field in the database.
+    // We need a way to change the parent folder, and all of its parent folders, to have a new hasNotes
+    // property. The best way to do this would be some form of recursion, so here I define a method
+    // which takes in a folder ID as a parameter.
     updateFolderHasNotes = folderId => {
-
-        // Base Case - If the ID of the folder passed in is the root
-        // folder, then we should stop.
+        
+        // First we create the base case, if we have reached the root folder (the folder with ID of 0)
+        // then we can return null to escape out the method and halt the recursion.
         if (folderId === 0) {
             return null;
         }
 
+        // Otherwise, we initiate a PUT request to the folders/has-notes endpoint so we can change the
+        // relevant field. Here we pass in the folder ID specified in the paramater and the new state
+        // of hasNotes.
         fetch('http://localhost:3000/folders/has-notes', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -85,18 +117,25 @@ export default class AddNotesCard extends React.Component {
                 hasNotes: true
             })
         })
+        // Converting the JSON response to a Javascript object.
         .then(response => response.json())
+        // Once done we can...
         .then(message => {
+            // If the API responds with a message of success, we can then call this method on the ID
+            // of the parent folder. This results in this recursion where all the parent folders will
+            // be updated.
             if (message.success) {
                 this.updateFolderHasNotes(message.parentFolderId)
             }
         })
+        // If there was an error at any point then we log it to the console for debugging.
         .catch(err => console.log(err));
 
     }
 
     render() {
 
+        // Getting the current folder, the user and the changeRoute method in the props.
         const { currentFolder, user, changeRoute } = this.props;
 
         return(
@@ -141,5 +180,4 @@ export default class AddNotesCard extends React.Component {
             </div>
         );
     }
-
 }
--- physics-notes-app/src/Components/NoteCard/NoteCard.js
+++ physics-notes-app/src/Components/NoteCard/NoteCard.js
@@ -12,11 +12,15 @@ import ForumCard from '../ForumCard/ForumCard';
 
 import './NoteCard.css';
 
+// Inheriting from React.Component.
 export default class NoteCard extends React.Component {
     
     constructor(props) {
         super(props);
 
+        // In the state we are storing the URL of the image, the instance of the User class that posted
+        // the notes, a boolean describing whether or not the Forum should be shown and finally a state
+        // for alerting the user if they couldn't delete the notes in the database.
         this.state = {
             imageUrl: null,
             author: null,
@@ -28,27 +32,41 @@ export default class NoteCard extends React.Component {
 
     }
 
+    // Once the component is mounted, we want to load the image from the API.
     componentDidMount() {
+        // First we get the instance of the Note class from the props.
         const { note } = this.props;
-
+        // Then we get the URL of the image and store it in getImage. To do this, we initiate a GET
+        // request to the notes/image endpoint.
         const getImage = fetch(`http://localhost:3000/notes/image/${note.id}`)
+            // Then we turn the response into a Javascript object.
             .then(response => response.json())
+            // With the newly created object...
             .then(data => {
 
+                // If the API responds with a success status of false, we throw a new error and tell
+                // the user that there was an error while getting the notes.
                 if (!data.success) {
                     throw Error('Failure while attempting to get notes');
                 }
 
+                // Otherwise, we return a Promise with the imageURL as a return value.
                 return new Promise((resolve, reject) => {
                     resolve(data.imageUrl);
                     reject('Failed to get notes');
                 })
             })
+            // If there was an error in our request to the API then we log the error to the console.
             .catch(err => console.log(err));
 
+        // Next we need the author the notes. To do this, we send a GET request to the users endpoint
+        // passing in the author ID of the notes that have been passed as a prop.
         const getAuthor = fetch(`http://localhost:3000/users/${note.authorId}`)
+            // Turning the JSON response into a Javascript object.
             .then(response => response.json())
+            // Using the object.
             .then(data => new Promise((resolve, reject) => {
+                // Creating the author, an instance of the User class.
                 const author = new User(
                     data.user.user_id,
                     data.user.first_name,
@@ -59,12 +77,16 @@ export default class NoteCard extends React.Component {
                     data.user.num_of_posts
                 );
                 
+                // Sending the author back in the Promise
                 resolve(author);
                 reject('Failed to create user');
 
             }))
+            // If there was an error, log it to the console.
             .catch(err => console.log(err));
 
+        // Once both of the above Promises have been resolved, we can then update the state using the
+        // data passed in by the Promises.
         Promise.all([getImage, getAuthor])
             .then(data => {
                 const [ imageUrl, author ] = data;
@@ -75,32 +97,44 @@ export default class NoteCard extends React.Component {
 
     }
 
+    // This is the method to handle the case that the user wants to delete the notes.
     deleteNote = () => {
 
+        // First we take in the notes and the changeRoute method from the props.
         const { note, changeRoute } = this.props; 
 
+        // Now we initiate a DELETE request to the /notes endpoint.
         fetch(`http://localhost:3000/notes/${note.id}`, {
             method: 'delete'
         })
+        // Getting the response as JSON and converting it to a Javascript object.
         .then(response => response.json())
         .then(data => {
+            // If the request was successful then we reroute the user to the index route.
             if (data.success) {
                 changeRoute('index');
             }
+            // Otherwise we update the state to alert the user that an error occured in the backend.
             else {
                 console.log(data.message)
                 this.setState({ failedToDeleteNotes: true });
             }
         })
+        // If any error occurs then we do the same thing.
         .catch(err => this.setState({ failedToDeleteNotes: true }));
 
     }
 
+    // The render function for the component.
     render() {
 
+        // We get the notes, the parent folder and the current user from the props.
         const { note, parentFolder, currentUser } = this.props;
+        // Then we destructure the author, URL of the image, the show forum boolean and the alert state.
         const { author, imageUrl, showForum, failedToDeleteNotes } = this.state;
 
+        // If the image URL is null or the author is null then the API has not yet responded and we
+        // show a loading screen to the user.
         if (imageUrl === null || author === null) {
             return(
                 <div className={'centralise'}>
@@ -109,7 +143,7 @@ export default class NoteCard extends React.Component {
                 </div>
             );
         }
-
+        // Otherwise, we return the component
         else {
             return(
                 <div>
@@ -168,7 +202,6 @@ export default class NoteCard extends React.Component {
                                         <Button variant='danger' onClick={() => this.setState({ showForum: true })}>Show Forum</Button>
                                     }
 
-
                                 </Card.Body>
                             </Card>
                         </Col>
@@ -177,7 +210,5 @@ export default class NoteCard extends React.Component {
                 </div>
             );
         }
-
     }
-
 }
