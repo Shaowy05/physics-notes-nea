18d529de1450a4d7f6d2b6be26d006ea95ef6903
Shaowy05 <jamesshao05@hotmail.com>
2023-03-15 21:25:16 +0000

    Annotation

--- physics-notes-app/src/Components/ForumCard/ForumCard.js
+++ physics-notes-app/src/Components/ForumCard/ForumCard.js
@@ -10,30 +10,44 @@ import QuestionCard from "../QuestionCard/QuestionCard";
 
 import './ForumCard.css';
 
+// Inheriting from React.Component.
 export default class ForumCard extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            // Here we have a state storing the array of questions.
             questionArray: null,
+            // A boolean for checking whether or not the questions have been fetched or not. This is
+            // used to render a loading screen if the questions haven't been loaded yet.
             questionsFetched: false,
 
+            // If the user is adding a question we need to render the page differently so we store a
+            // boolean to represent this as well.
             addingQuestion: false,
+            // The input field for the question title.
             questionTitle: '',
+            // The input field for the text.
             questionText: '',
+            // If there was a failure during adding the question then we want to alert the user.
             failedToAddQuestion: false
         }
     }
 
+    // Update methods for the question title and text states.
     updateQuestionTitle = event => this.setState({ questionTitle: event.target.value });
-
     updateQuestionText = event => this.setState({ questionText: event.target.value });
 
+    // The mthod for posting the question, run upon the user pressing the submit button.
     postQuestion = () => {
 
+        // Getting the current user and the parent note from the props.
         const { currentUser, parentNote } = this.props;
+        // Getting the title and the text from the state.
         const { questionTitle, questionText } = this.state;
 
+        // Now we send a POST request to the questions endpoint in the API. Passing in the title, the
+        // text, the ID of the current user and the ID of the note that they have posted to.
         fetch('http://localhost:3000/questions', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
@@ -44,32 +58,54 @@ export default class ForumCard extends React.Component {
                 noteId: parentNote.id
             })
         })
+        // Converting the response from JSON to a Javascript object.
         .then(response => response.json())
+        // With this response...
         .then(data => {
+            // If the request was successful, then we set the adding question state to be false and
+            // we set the failed to add question state to be false so that any errors that may have
+            // occurred recently are wiped.
             if (data.success) {
                 this.setState({ addingQuestion: false, failedToAddQuestion: false });
+            // Otherwise we update the failed to add question state to be true so that the user has
+            // visual feedback.
             } else {
                 this.setState({ failedToAddQuestion: true });
             }
         })
+        // If there was an error at any point we log it to the console.
         .catch(err => console.log(err));
 
+        // Finally we return null to escape out of the function.
         return null;
 
     }
 
+    // Once the component has mounted, we want to get all the questions ready for if the User wants
+    // view the forum. This provides the quickest way to provide the user with visual feedback.
     componentDidMount() {
 
+        // Getting the parent note from the props.
         const { parentNote } = this.props;
 
+        // Using the ID of the note, we can send a GET request to the questions endpoint, passing in
+        // the ID as a paramter so that we can get all the questions relevant to the set of notes that
+        // they are viewing.
         fetch(`http://localhost:3000/questions/note-id=${parentNote.id}`)
+            // Converting the JSON to Javascript.
             .then(response => response.json())
             .then(data => {
+                // If the request was not successful we return null to escape out of the function. This
+                // leaves the user with the loading screen to show that something has gone wrong.
                 if (!data.success) {
                     return null;
                 }
 
+                // Otherwise, we can use the questions retrieved from the database so that we can create
+                // the question array to put in the state. We do this by using the .map method on the
+                // questions passed in.
                 const questionArray = data.questions.map(question => {
+                    // For each question we return a new instance of the Question class.
                     return new Question(
                         question.question_id,
                         question.question_title,
@@ -79,19 +115,25 @@ export default class ForumCard extends React.Component {
                         question.upload_date
                     );
                 })
-
+                // Once the question array has been created we can add it to the state, and we set the
+                // questionsFetched state to be true so that the user doesn't see the loading screen
+                // anymore.
                 this.setState({ questionArray: questionArray, questionsFetched: true });
 
             })
+            // If there was an error at any point we log to the console.
             .catch(err => console.log(err));
-
     }
 
+    // The render method for the Forum Card component.
     render() {
 
+        // Getting the relevant info from the state.
         const { questionArray, questionsFetched, addingQuestion, failedToAddQuestion } = this.state;
+        // Getting the current user from the props so that we can pass it into the responses.
         const { currentUser } = this.props;
 
+        // If the questions have been fetched then we can safely return the questions to the user.
         if (questionsFetched) {
             return (
                 <div>
@@ -127,7 +169,7 @@ export default class ForumCard extends React.Component {
                 </div>
             );
         }
-
+        // Otherwise, the questions haven't been properly fetched yet so we render a loading screen.
         else {
             return (
                 <div className={'centralise'}>
@@ -136,7 +178,5 @@ export default class ForumCard extends React.Component {
                 </div>
             );
         }
-
     }
-
 }
--- physics-notes-app/src/Components/QuestionCard/QuestionCard.js
+++ physics-notes-app/src/Components/QuestionCard/QuestionCard.js
@@ -10,35 +10,55 @@ import Alert from 'react-bootstrap/Alert';
 import './QuestionCard.css';
 import ResponseCard from "../ResponseCard/ResponseCard";
 
+// Inheriting from React.Component.
 export default class QuestionCard extends React.Component {
 
     constructor(props) {
         super(props);
 
+        // Getting the question from the props.
         const { question } = this.props;
 
         this.state = {
+            // We load the question into the state.
             question: question,
 
+            // We have a boolean for whether or not the user is creating a response.
             creatingResponse: false,
+            // A state for storing the text for the response.
             responseText: '',
+            // A boolean for the case that the user has failed to create a response.
             failedToCreateResponse: false
         }
     }
 
+    // Method to update the text in the state.
     updateResponseText = event => this.setState({ responseText: event.target.value })
 
+    // This method handles any form of voting that the user does to any of the responses, whether it
+    // be an upvote or a downvote. Here we pass in the click event to the method so that we can use
+    // the ID to identify which response was voted on.
     updateVote = event => {
 
+        // First we load the current user and the question into the props.
         const { currentUser, question } = this.props;
 
+        // Now we need to split the ID into two parts: the voting type and the ID of the response. We
+        // do this by using the .split method to get the two parts of the string.
         const parsedTagId = event.target.split('-');
 
+        // Then we load the voting type into the table variable.
         const table = parsedTagId[0];
+        // And the ID into responseId.
         const responseId = parsedTagId[1];
 
+        // Now we use a ternary operator on the upvotedByCurrentUser property to decide whether or not
+        // we should increment or decrement the value in the table. Here we utilise the getResponseById
+        // method to get the response.
         const option = (question.getResponseById(responseId).upvotedByCurrentUser) ? 'decrement' : 'increment';
 
+        // Now we send two GET requests to the API. One to update the voting on the responses in the
+        // response table...
         fetch('http://localhost:3000/responses/votes' , {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -48,7 +68,8 @@ export default class QuestionCard extends React.Component {
                 option: option
             })
         })
-
+        // And another to add a relation in the relevant link table for the current user and the response
+        // that they voted on.
         fetch('http://localhost:3000/votes', {
             method: (option === 'increment') ? 'post' : 'delete',
             headers: {'Content-Type': 'application/json'},
@@ -58,16 +79,22 @@ export default class QuestionCard extends React.Component {
                 table: table
             })
         })
-
     }
 
+    // This methods handles the event that a user wants to add a response to the question.
     createResponse = () => {
 
+        // First we get the qusetion and the response text from the state.
         const { question, responseText } = this.state;
+        // And we also need the current user which is passed in from the props.
         const { currentUser } = this.props;
 
+        // If the response text has nothing inside it then we want to return null, as this is an invalid
+        // response.
         if (responseText === '') { return null }
 
+        // Then we send a POST request to the responses endpoint in the API, passing in the text of 
+        // the response, the current user's ID and the ID of the question in the body of the request.
         fetch('http://localhost:3000/responses', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
@@ -77,47 +104,69 @@ export default class QuestionCard extends React.Component {
                 questionId: question.id
             })
         })
+        // Once we have a response, we convert the JSON to a Javascript object.
         .then(response => response.json())
         .then(data => {
+            // If the request was successful then we want to update the state to say that we are no
+            // longer creating a response and also clear any error messages that may have appeared.
             if (data.success) {
                 this.setState({ creatingResponse: false, failedToCreateResponse: false });
             }
+            // Otherwise we update the state so that an error message is shown.
             else {
                 this.setState({ failedToCreateResponse: true });
             }
         })
+        // If at any point during the GET request there was an error, then we display the alert message.
         .catch(err => this.setState({ failedToCreateResponse: true }));
 
+        // Return to escape out the function.
         return null;
 
     }
 
+    // Once the component has mounted we want to get all the responses from the responses table.
     componentDidMount() {
 
+        // Loading the question from the state.
         const { question } = this.state;
+        // Loading the current user from the props. This is needed in order to show the state of the
+        // voting for this user, e.g. if they have already voted then we want to display that.
         const { currentUser } = this.props;
 
+        // Sending a GET request to the responses endpoint in the API with the question ID as a parameter.
         fetch(`http://localhost:3000/responses/question-id=${question.id}`)
             .then(response => response.json())
             .then(data => {
+                // If the request was not successful then we escape out the function to prevent any
+                // errors from occuring.
                 if (!data.success) {
                     return null;
                 }
 
+                // We create a temporary copy of the question in the state.
                 const tempQuestion = question;
+                // For each of the responses that have been fetched from the API...
                 data.responses.forEach(response => {
 
+                    // We create variables to store whether or not the user has voted on this response
+                    // for both upvoting and downvoting. We do this by using the getUpvoteResponseId
+                    // method to try and find the response.
                     let upvotedByCurrentUser = currentUser.getUpvoteResponseId(response.response_id);
                     let downVotedByCurrentUser = currentUser.getDownvoteResponseId(response.response_id);
 
+                    // If there is a response, then we want to assign true to the upvotedByCurrentUser
+                    // so that we can display the fact that the current user has already voted.
                     if (upvotedByCurrentUser !== false) {
                         upvotedByCurrentUser = true;
                     }
-
+                    // The same goes for downvoting.
                     if (downVotedByCurrentUser !== false) {
                         downVotedByCurrentUser = true;
                     }
 
+                    // Once all of this logic has been performed we can add a new instance of the Response
+                    // class with the booleans that we created earlier.
                     tempQuestion.addResponse(new Response(
                         response.response_id,
                         response.response_text,
@@ -131,17 +180,23 @@ export default class QuestionCard extends React.Component {
                     ));
                 });
 
+                // Finally we update the state with the new question that we have created.
                 this.setState({ question: tempQuestion });
-
             })
-
     }
 
+    // The render method for Question Card.
     render() {
 
+        // Here we get the question, the creatingResponse boolean and the failedToCreateResponse boolean
+        // from the state.
         const { question, creatingResponse, failedToCreateResponse } = this.state;
+        // Then we get the user from the props.
         const { currentUser } = this.props;
 
+        // Now we utilise the getOrderedResponses method on the question to get all of the responses
+        // in the order we require, i.e. solutions should be at the top and the other responses should
+        // be ordered based off of the formula created earlier.
         const orderedResponses = question.getOrderedResponses();
 
         return (
@@ -182,7 +237,5 @@ export default class QuestionCard extends React.Component {
                 </Card>
             </div>
         );
-
     }
-
 }
